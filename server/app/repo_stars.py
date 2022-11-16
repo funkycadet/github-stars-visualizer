26ca6da583b2aa28db0ddfadbddaf91b10358434
funkycadet <ericsmithsonian2@gmail.com>
2022-11-16 16:45:59 +0100

    [update]: updated the language endpoints

--- server/app/repo_stars.py
+++ server/app/repo_stars.py
@@ -1,34 +1,85 @@
 #!/usr/bin/env python3
+"""
+This module contains the code for rendering the starred
+projects on GitHub
+"""
 
-from flask import abort, jsonify, request, make_response
 from app import app
+from flask import abort, jsonify, request, make_response
+from plotly.graph_objs import Bar
 import requests
 import sys
 
-# Make an API call and store the response
+
 @app.route("/language/<language>", methods=["GET"], strict_slashes=True)
 def get_stars_by_language(language):
     """ This method returns top starred projects by language """
-    url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+
     url = 'https://api.github.com/search/repositories?q=language:{}&sort=stars'.format(language)
-    header = {'Accept': 'application/vnd.github.v3+json'}
+    header = {'Accept': 'application/vnd.github+json'}
 
     r = requests.get(url, headers=header)
-    print("Status code: {}".format(r.status_code))
 
-    # Store API response in a variable
     response_dict = r.json()
-    print(f"Total repositories: {response_dict['total_count']}")
-
-    # Explore information about the repositories.
     repo_dicts = response_dict['items']
-    print(f"Repositories returned: {len(repo_dicts)}")
+    # repo_dict = repo_dicts[0]
 
-    # Examine the first repository
-    repo_dict = repo_dicts[0]
-    print("\nKeys: {}".format(len(repo_dict)))
-    for key in sorted(repo_dict.keys()):
-        print(key)
+    repo_links, stars, labels = [], [], []
+
+    for repo_dict in repo_dicts:
+        repo_name = repo_dict['name']
+        repo_url = repo_dict['html_url']
+        repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+        repo_links.append(repo_link)
+
+        stars.append(repo_dict['stargazers_count'])
+
+        owner = repo_dict['owner']['login']
+        description = repo_dict['description']
+        label = f"{owner}<br />{description}"
+        labels.append(label)
+
+    # for key in sorted(repo_dict.keys()):
+    #     keys = key
+
+    # Make visualization
+    data = [{
+        'type': 'bar',
+        'x': repo_links,
+        'y': stars,
+        'hovertext': labels,
+        'marker': {
+            'color': 'rgb(60, 100, 150)',
+            'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+        },
+        'opacity': 0.6,
+    }]
+
+    data_layout = {
+        'title': 'GitHub Stars Visualizer',
+        'titlefont': {'size': 28},
+        'xaxis': {
+            'title': 'Repository',
+            'titlefont': {'size': 24},
+            'tickfont': {'size': 14},
+        },
+        'yaxis': {
+            'title': 'Stars',
+            'titlefont': {'size': 24},
+            'tickfont': {'size': 14},
+        },
+    }
 
     # Process result
-    return(response_dict.keys())
+    # keys = response_dict.keys()
+    # result = [
+    #     "Status code: {}".format(r.status_code),
+    #     f"Total repositories: {response_dict['total_count']}",
+    #     f"Repositories returned: {len(repo_dicts)}",
+    #     f"Keys: {len(repo_dict)}",
+    #     f"{keys}"
+    # ]
+    # return(result)
+
+    figure = {'data': data, 'layout': data_layout}
+    return figure
