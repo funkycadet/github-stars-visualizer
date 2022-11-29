8a2b8827e5060c3cbc9d34d028996ff6a9e69970
Vodina Efem <efem.vodina@gmail.com>
2022-11-29 20:06:11 +0100

    landing page demo

--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -4,14 +4,15 @@ import React from 'react'
 export const LandingPage = () => {
   return (
     <div>
-          <div className='hero-section h-screen flex flex-col'>
-              <h1>
-                  A better way to explore yout favourite programming languages
+          <div className='hero-section h-screen space-y-6'>
+              <h1 className='text-3xl font-bold'>
+                  A better way to explore your favourite programming languages
               </h1>
               <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum non consectetur a erat nam at lectus.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br> sed do eiusmod tempor incididunt ut labore et dolore magna 
+                  <br></br>aliqua. Dictum non consectetur a erat nam at lectus.
               </p>
-              <button>
+              <button id='explore-button'>
                   Get Started
               </button>
           </div>
