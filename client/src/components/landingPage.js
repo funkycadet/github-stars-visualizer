4eac9e0554126c682bf324eb2b29037708e2b0ea
Simplyvoda <efem.vodina@gmail.com>
2022-12-01 23:40:08 +0100

    landing page

--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import search from '../images/search.svg'
-import explore from '../images/explore.svg'
-import learn from '../images/learn.svg'
-import line from '../images/horizontal-line.svg'
-
+// images for hero section
+import search from '../images/search.svg';
+import explore from '../images/explore.svg';
+import learn from '../images/learn.svg';
+import line from '../images/horizontal-line.svg';
+// images for graph section
+import bar from '../images/bar-chart.svg';
+import linechart from '../images/line-chart.svg';
+import pie from '../images/pie-chart.svg';
 
 
 
@@ -19,32 +23,23 @@ export const LandingPage = () => {
           Get Started
         </button>
       </article>
-
-      {/* Get Started section */}
-      <article className='get-started flex justify-center flex-col items-center w-100% space-y-6 p-8'>
-        <h1 className='text-5xl font-bold'>
-          Get started in a few minutes
+      {/* About us section */}
+      <div className='about-section'>
+        <h1 className='text-center text-3xl'>
+          Visualise your preferred language by repositories
         </h1>
-        <p className='text-xl'>
-          Explore over 90+ repositories from GitHub
-        </p>
-        <div className='flex flex-row justify-evenly items-center w-100% space-x-16'>
-          <div className='space-y-6 flex flex-col justify-center items-center text-center'>
-            <img src={search} alt="search" height={90} width={90} fill="red"/>
-            <h1 className=' text-2xl'>Search</h1>
+        <article className='flex justify-evenly flex-row items-center w-90% space-x-4 p-10'>
+          <div className='graph-container flex justify-center items-center'>
+            <img src={bar} alt="bar-chart" height={150} width={150} />
           </div>
-          <img src={line} alt="line"/>
-          <div className='space-y-6 flex flex-col justify-center items-center text-center'>
-            <img src={explore} alt="explore" height={90} width={90}/>
-            <h1 className=' text-2xl'>Explore</h1>
+          <div className='graph-container flex justify-center items-center'>
+            <img src={linechart} alt="line-chart" height={150} width={150} />
           </div>
-          <img src={line} alt="line" />
-          <div className='space-y-6 flex flex-col justify-center items-center text-center'>
-            <img src={learn} alt="learn" height={90} width={90}/>
-            <h1 className='text-2xl'>Learn more</h1>
+          <div className='graph-container flex justify-center items-center'>
+            <img src={pie} alt="pie-chart" height={150} width={150} />
           </div>
-        </div>
-      </article>
+        </article>
+      </div>
       {/* Small Feature description section */}
       <article className='feature-section flex justify-center flex-col items-center w-100% space-y-4 p-8 relative'>
         <p className='feature-paragraph text-3xl text-white text-center'>
@@ -55,12 +50,37 @@ export const LandingPage = () => {
       </article>
       <div class="blankDiv">
       </div>
-      {/* About us section */}
-      <article className='about-section flex justify-center flex-row items-center w-100% space-x-4'>
-        <div></div>
-        <div></div>
-        <div></div>
+      {/* Get Started section */}
+      <article className='get-started flex justify-center flex-col items-center w-100% space-y-6 p-8'>
+        <h1 className='text-5xl font-bold'>
+          Get started in a few minutes
+        </h1>
+        <p className='text-xl'>
+          Explore over 90+ repositories from GitHub
+        </p>
+        <div className='flex flex-row justify-evenly items-center w-100% space-x-16'>
+          <div className='space-y-6 flex flex-col justify-center items-center text-center'>
+            <img src={search} alt="search" height={90} width={90} fill="red" />
+            <h1 className=' text-2xl'>Search</h1>
+          </div>
+          <img src={line} alt="line" />
+          <div className='space-y-6 flex flex-col justify-center items-center text-center'>
+            <img src={explore} alt="explore" height={90} width={90} />
+            <h1 className=' text-2xl'>Explore</h1>
+          </div>
+          <img src={line} alt="line" />
+          <div className='space-y-6 flex flex-col justify-center items-center text-center'>
+            <img src={learn} alt="learn" height={90} width={90} />
+            <h1 className='text-2xl'>Learn more</h1>
+          </div>
+        </div>
       </article>
+      <div className='text-center'>
+        <button className='explore-button'>
+          Get Started
+        </button>
+      </div>
+
     </div>
   )
 }
